5e8fcbe1aebc74aa9ff9a9a3e335f427e20913c8
akrabatt <starwars-1999@yandex.ru>
2025-04-06 01:47:08 +0200

    add new defenition

--- sys_ftruncate/main.c
+++ sys_ftruncate/main.c
@@ -10,6 +10,11 @@ int main()
 	off_t file_size = 0;
 	off_t new_file_size = 0;
 	
+	const char *file_name = "./f1.txt";
+	off_t new_size = 150;
+	
+#ifdef FTRUNCATE
+
 	// opening file
 	if((fd = open("./f1.txt", O_RDWR)) == -1) { perror("Error open f1.txt\n"); exit(EXIT_FAILURE); }
 	printf("File opened...\n");	
@@ -22,7 +27,20 @@ int main()
 	new_file_size = file_size / 2;
 
 	if(ftruncate(fd, new_file_size) == -1) { perror("Error truncating file\n"); close(fd); exit(EXIT_FAILURE); }
+	printf("File ftruncated successfully\n");
 
 	// close file
 	close(fd);
+#else
+
+	if(truncate(file_name, new_size) == -1)
+	{
+		perror("Error truncating file\n");
+		return EXIT_FAILURE;
+	}
+	printf("File '%s' truncated to %ld bytes\n", filename, new_size);
+
+#endif
+
+	return 0;
 }
